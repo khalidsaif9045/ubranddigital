babd28aa1b2ffe622faf374d48401f3e2100b72a
Khalid Saifullah <khalidsaif9045@gmail.com>
2025-06-22 02:03:41 +0530

    deployment

--- next.config.js
+++ next.config.js
@@ -1,4 +1,8 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
-
-module.exports = nextConfig;
+const nextConfig = {
+    output: 'export',
+    trailingSlash: true,
+  };
+  
+  module.exports = nextConfig;
+  
