e95e568111f6c27c6d2707522293968065df7ec9
Khalid Saifullah <khalidsaif9045@gmail.com>
2025-09-14 03:23:36 +0530

    api key removed

--- app/api/sendMail/route.ts
+++ app/api/sendMail/route.ts
@@ -1,7 +1,7 @@
 import { NextResponse } from "next/server";
 import { Resend } from "resend";
 
-const resend = new Resend("re_cYq4QCqp_NNxQY9qtGfZhMrUbK48Wb6UJ");
+const resend = new Resend("");
 
 export async function POST(req: Request) {
   try {
