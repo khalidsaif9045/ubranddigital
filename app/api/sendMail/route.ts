23c6e413f57ea979a7a2a7ed2993de2c4bf69cf9
Khalid Saifullah <khalidsaif9045@gmail.com>
2025-09-14 12:03:30 +0530

    backup key added

--- app/api/sendMail/route.ts
+++ app/api/sendMail/route.ts
@@ -1,7 +1,7 @@
 import { NextResponse } from "next/server";
 import { Resend } from "resend";
 
-const resend = new Resend("");
+const resend = new Resend("re_123");
 
 export async function POST(req: Request) {
   try {
